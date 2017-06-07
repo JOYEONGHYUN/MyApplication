b66f0cf274de72c26687ad078988de4c27bcdabb
JOYEONGHYUN <oeam1212@gmail.com>
2017-06-07 09:39:16 +0900

    Update FriendAdapter.java

--- app/src/main/java/org/androidtown/myapplication/FriendAdapter.java
+++ app/src/main/java/org/androidtown/myapplication/FriendAdapter.java
@@ -10,9 +10,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.squareup.picasso.Picasso;
-
 import java.util.List;
 public class FriendAdapter extends RecyclerView.Adapter<FriendAdapter.ViewHolder> {
 
