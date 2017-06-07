f403286e3c9ee1f5365a598491b4beb98f0a3fb4
JOYEONGHYUN <oeam1212@gmail.com>
2017-06-07 09:39:50 +0900

    Update FriendsFragment.java

--- app/src/main/java/org/androidtown/myapplication/FriendsFragment.java
+++ app/src/main/java/org/androidtown/myapplication/FriendsFragment.java
@@ -8,13 +8,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
 import java.util.ArrayList;
 import java.util.List;
 
